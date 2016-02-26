6f5a7d0979c486076d222adaae05ae6bf39685c8
DigitalChiller <DigitalChiller@users.noreply.github.com>
2016-02-26 13:59:49 +0100

    licenseing

--- omnimaga/plugins/permission/modifier.py
+++ omnimaga/plugins/permission/modifier.py
@@ -1,18 +1,18 @@
 ## LICENSE
-#    This file is part of my ircbot.
+#    This file is part of nBot.
 #
-#    my ircbot is free software: you can redistribute it and/or modify
+#    nBot is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    my ircbot is distributed in the hope that it will be useful,
+#    nBot is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with my ircbot.  If not, see <http://www.gnu.org/licenses/>.
+#    along with nBot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 uperms = self.permM.perms(msg["ident"].lower())
