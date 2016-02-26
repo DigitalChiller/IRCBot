835c1422b768029e6d5243b90deccee859ab53df
DigitalChiller <DigitalChiller@users.noreply.github.com>
2016-02-26 08:52:11 +0100

    Update __init__.py

--- omnimaga/plugins/help/__init__.py
+++ omnimaga/plugins/help/__init__.py
@@ -1,19 +1,16 @@
-try:
-	#create files and stuff
-	self.infotext = "this extension is a template"
-
-	
-	self.helpcmd = {}
-	self.helpcmd["all"] = ["list of commands"]
-	self.helpcmd[":cmd"] = {"syntax":":cmd <syntax>","info":"that command does not exist"}
-	
-	self.helpperm = {}
-	self.helpperm["all"] = ["list of all permissions except all"]
-
-	self.settings = {"all":[]} #TODO
-
-	self.handles = ["user"]
-
-except:
-	#fail, report it
-	traceback.print_exc()
+## LICENSE
+#    This file is part of my ircbot.
+#
+#    my ircbot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    my ircbot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with my ircbot.  If not, see <http://www.gnu.org/licenses/>.
+#
